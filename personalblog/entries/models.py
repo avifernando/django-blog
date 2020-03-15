36b21cb46546cdb3814073095bcc8f820793c27f
avifernando <avifernando16@gmail.com>
2020-03-15 16:25:16 -0600

    agregando un editor de texto enriquecido a django CKEDITOR, y safe para poder ver correctamente las funciones de estiloes en los textos

--- personalblog/entries/models.py
+++ personalblog/entries/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from ckeditor.fields import RichTextField
 # Create your models here.
 class entry(models.Model):
     entry_title = models.CharField(max_length=50)
-    entry_test = models.TextField()
+    entry_test = RichTextField(blank = True, null = True)
     entry_date = models.DateTimeField(auto_now_add=True)
     entry_author = models.ForeignKey(User, on_delete=models.CASCADE)
 
