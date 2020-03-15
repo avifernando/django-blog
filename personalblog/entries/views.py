c749cb54d04858f19c54a7711ea9710f104e0875
avifernando <avifernando16@gmail.com>
2020-03-14 21:54:36 -0600

    creando el url para agregar la vista, y agregando el template html para listar los posts

--- personalblog/entries/views.py
+++ personalblog/entries/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import entry
 
 # Create your views here.
+class HomeView(ListView):
+    model = entry
+    template_name='entries/index.html'
