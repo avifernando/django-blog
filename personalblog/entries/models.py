e727b0bd4acf41ceeaeb2700bc5267be98fb55b9
avifernando <avifernando16@gmail.com>
2020-03-14 20:31:49 -0600

    Creando clase para que las entradas en el admin no se vean como un objeto de datos, y aparescan con el titulo corespondiente

--- personalblog/entries/models.py
+++ personalblog/entries/models.py
@@ -8,3 +8,10 @@ class entry(models.Model):
     entry_date = models.DateTimeField(auto_now_add=True)
     entry_author = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        return f'{self.entry_title}'
+        
+
