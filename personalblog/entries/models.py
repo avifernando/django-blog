2c795a573abbfa682328950ac7a36dd5d207d614
avifernando <avifernando16@gmail.com>
2020-03-14 20:06:08 -0600

    agregando la app entries a setting, creando el modelo de la BD a utilizar

--- personalblog/entries/models.py
+++ personalblog/entries/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class entry(models.Model):
+    entry_title = models.CharField(max_length=50)
+    entry_test = models.TextField()
+    entry_date = models.DateTimeField(auto_now_add=True)
+    entry_author = models.ForeignKey(User, on_delete=models.CASCADE)
+
